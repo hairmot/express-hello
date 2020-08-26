37302f13363ba28e3e2f8b17c373d672b8cf6c74
Rohit Bohara <r.bohara@digital-worx.de>
2020-08-26 15:34:49 +0200

    add check app status file

---
+++ check-app-status.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+HOST_NAMES=("madr" "indexing" "ranking", "orchestrator", "keyrock", "pap-pdp", "pep-proxy", "security-facade")
+HOST_URLS=("https://mdr.iotcrawler.eu/actuator/health"
+            "https://indexing.iotcrawler.eu/"
+            "https://ranking.iotcrawler.eu/"
+            "https://orchestrator.iotcrawler.eu/"
+            "https://keyrock.iotcrawler.eu/"
+            "https://pap-pdp.iotcrawler.eu/XACMLServletPDP"
+            "https://pep-proxy.iotcrawler.eu/"
+            "https://security-facade.iotcrawler.eu/CapabilityManagerServlet"
+          )
+function checkStatus() {
+  name=$1
+  url=$2
+  code=$(curl -sI -w "%{http_code}\n" $url -o /dev/null)
+  if [[ $code == 200 ]]
+  then  
+    echo "$name is online"
+  else
+    echo "$name is offline "
+  fi
+}
+
+for name in ${!HOST_NAMES[@]}
+do
+  checkStatus ${HOST_NAMES[$name]} ${HOST_URLS[$name]}
+done
